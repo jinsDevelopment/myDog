60830b53174e80212fb812d8e238a05e93a688aa
Juhwan <dev.juhwanlee@gmail.com>
2022-03-07 18:05:58 +0900

    Create initial community html

--- app.py
+++ app.py
@@ -11,5 +11,9 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/community')
+def community():
+    return render_template('community.html')
+
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
