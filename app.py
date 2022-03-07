da0a0c1623e12a39b3602e7246d684f84e5d95de
Juhwan <dev.juhwanlee@gmail.com>
2022-03-07 16:27:16 +0900

    Create initial app py

---
+++ app.py
@@ -0,0 +1,15 @@
+from pymongo import MongoClient
+#import certifi
+#mongo_connect = 'mongodb+srv://test:sparta@cluster0.u9lvb.mongodb.net/Cluster0?retryWrites=true&w=majority'
+#client = MongoClient(mongo_connect,tlsCAFile=certifi.where())
+#db = client.dbsparta
+
+from flask import Flask, render_template, request, jsonify
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
