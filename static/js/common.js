2077aed525fa5c1fb5467769cc156cae92f15e8c
Juhwan <dev.juhwanlee@gmail.com>
2022-03-08 20:55:28 +0900

    Divide script: create common js

---
+++ static/js/common.js
@@ -0,0 +1,23 @@
+'use strict';
+// 재혁님 작업
+function logout() {
+  $.removeCookie('mytoken');
+  alert('로그아웃!');
+  window.location.href = '/login';
+}
+
+function boardList() {
+  window.location.href = '/boardList';
+}
+
+function goLogin() {
+  window.location.href = '/login';
+}
+function goJoin() {
+  window.location.href = '/join';
+}
+
+function write_page() {
+  window.location.href = '/write';
+}
+//
