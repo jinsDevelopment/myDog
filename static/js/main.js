0c5c92adb03ed3b1ecb97306949d5262397b52a7
Juhwan <dev.juhwanlee@gmail.com>
2022-03-08 17:29:10 +0900

    Add function, Edit css html: modal

--- static/js/main.js
+++ static/js/main.js
@@ -25,3 +25,19 @@ function write_page() {
 /***********************************
  * Modal
  ***********************************/
+// Put information received from db in modal window
+const exampleModal = document.getElementById('exampleModal');
+exampleModal.addEventListener('show.bs.modal', function (event) {
+  // Button that triggered the modal
+  const button = event.relatedTarget;
+  // Extract info from data-bs-* attributes
+  const recipient = button.getAttribute('data-bs-whatever');
+  const thumbImg = button.getAttribute('data-img');
+
+  // Update the modal's content.
+  const modalTitle = exampleModal.querySelector('.modal-title');
+  const modalImg = document.getElementById('modal-img');
+
+  modalTitle.textContent = recipient;
+  modalImg.src = thumbImg;
+});
